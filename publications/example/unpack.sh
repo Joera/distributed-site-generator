d95a024a5a228014617794c6b302c3ee4e1ff8f2
Joera Mulders <joera@pop-os.localdomain>
2024-02-20 11:29:43 +0100

    all together

---
+++ publications/example/unpack.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+rm -rf /home/joera/Documents/dsg-publications/unamore/public/*
+/home/joera/.cargo/bin/car-utils ex -c /home/joera/Documents/dsg-publications/unamore/archive.car -t /home/joera/Documents/dsg-publications/unamore/public/
